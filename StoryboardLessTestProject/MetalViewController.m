ccd5bd4bb13c392a4871bc096c0054c33302043c
James Alan Bush <jamesalanbush1972@gmail.com>
2021-09-04 13:15:47 -0400

    Removed storyboard and replaced with programmatic UI

--- StoryboardLessTestProject/MetalViewController.m
+++ StoryboardLessTestProject/MetalViewController.m
@@ -38,14 +38,4 @@
     renderer = [[MetalRenderer alloc] initWithMetalKitView:self.view];
 }
 
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
-
 @end
